fec4250200be2e7c6bda80f4367cfbc655b8aab2
frsnic <frsnic@gmail.com>
2016-04-27 22:15:19 +0800

    backend accordion

--- app/assets/javascripts/backend_application.js
+++ app/assets/javascripts/backend_application.js
@@ -4,6 +4,4 @@
 
 $(document).on('ready page:load', function() {
 
-  $("#sidebar_nav li.active").parent().parent().addClass('open');
-
 });
--- app/helpers/backend/dashboard_helper.rb
+++ app/helpers/backend/dashboard_helper.rb
@@ -2,9 +2,9 @@ module Backend::DashboardHelper
 
   def nav_link(text, path, options = {})
     icon_tag = options[:icon] ? content_tag(:span, "", class: options[:icon]) : ""
-    content_tag(:li, current_page?(path) ? { class: "active" } : {}) do
-      link_to "#{text}#{icon_tag}".html_safe, path, data: options[:data]
-    end
+    active_icon = content_tag(:span, "", class: "glyphicon glyphicon-ok pull-right")
+    text = current_page?(path) ? "#{icon_tag}#{text}#{active_icon}" : "#{icon_tag}#{text}"
+    link_to text.html_safe, path, data: options[:data], class: (current_page?(path) ? "text-success" : "")
   end
 
 end
