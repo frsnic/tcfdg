dd032429e6eb1de06961cc1a9869679080c1b23b
frsnic <frsnic@gmail.com>
2016-05-21 13:23:33 +0800

    precompile font

--- app/controllers/frontend/activities_controller.rb
+++ app/controllers/frontend/activities_controller.rb
@@ -1,11 +1,11 @@
 class Frontend::ActivitiesController < Frontend::ApplicationController
 
   def index
-    @activities = Activity.page params[:page]
+    @activities = Activity.is_public.page params[:page]
   end
 
   def show
-    @activity = Activity.find params[:id]
+    @activity = Activity.is_public.find params[:id]
     redirect_to root_path, :flash => { error: "目錄已移除" } and return unless @activity
   end
 
