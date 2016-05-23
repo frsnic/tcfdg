5c8a44fd386ad4da4a7a4aa1c2f770e930708e5c
frsnic <frsnic@gmail.com>
2016-05-23 14:31:40 +0800

    production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,13 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # ActionMailer settings
+  config.action_mailer.default_url_options = { :host => 'taichung-fieldwork.org' }
+  config.action_mailer.smtp_settings = Setting.smtp.symbolize_keys
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = Setting.raise_delivery_errors
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,9 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # default_url_options
-  config.action_mailer.default_url_options = { :host => 'taichung-fieldwork.org' }
 
   # serve_static_files
   # config.serve_static_files = true
