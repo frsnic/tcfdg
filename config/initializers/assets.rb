3546c20cf371e25dad228e1d04e860b4982b7050
Hank <hank.shen@itrue.com.tw>
2017-02-11 12:56:24 +0800

    assets precompile add ckeditor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w(
+  ckeditor/* ckeditor/lang/*
   *.css *.js *gif *jpg *jpeg *png *woff *ttf *woff2 *eot *svg
 )
