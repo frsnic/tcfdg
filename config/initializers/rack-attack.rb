6584fd71b0951f0469de02bd7101718f3b961462
Hank <hank.shen@itrue.com.tw>
2017-02-11 12:45:45 +0800

    fix to many request

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -24,8 +24,8 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', :limit => 10, :period => 20.seconds) do |req|
-    req.ip # unless req.path.start_with?('/assets')
+  throttle('req/ip', :limit => 5, :period => 20.seconds) do |req|
+    req.ip unless req.path.start_with?('/assets')
   end
 
   ### Prevent Brute-Force Login Attacks ###
